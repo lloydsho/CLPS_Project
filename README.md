cb1642a4899a5cee49cc06307edf52893d17a84b
lloydsho <162610192+lloydsho@users.noreply.github.com>
2024-04-24 21:02:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CLPS_Project
+ 
---
+++ subject_data.m
@@ -0,0 +1,5 @@
+subject.name = input('What is your name?: ', 's');
+subject.age = input('What is your age?:');
+subject.gender = input('What is your gender?: ', 's');
+
+
